49eebb3c86857b2ddb2141b177b85a614dec4c79
joaovictor720 <silvajoao720@gmail.com>
2023-08-04 10:07:34 -0300

    one possible solution (tokenized)

--- criminal-search/src/main/java/com/testrigor/Main.java
+++ criminal-search/src/main/java/com/testrigor/Main.java
@@ -1,5 +1,6 @@
 package com.testrigor;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,11 +15,15 @@ public class Main {
         Map<String, String> criminals = new HashMap<>();
         criminals.put("Paul White Jr.", null);
         criminals.put("Paul White", "Roger Night, Peter Llong Jr.");
-        criminals.put("Roger Fedexer", "Rob Ford, Pete Lord, Roger McWire");
-        //criminals.put("Paul White Jr.", null);
         criminals.put("Red Fortress", "Roger Rabbit, Ross Winter");
-        criminals.put("Redford Fort", "Red Strong, Red Fort");
-        //criminals.put("Redford Fort", "Red Strong, Red Fort");
+        criminals.put("Redford Fort", "Red Strong, Red Fort, Roger Fordson");
+        criminals.put("Roger Fedexer", "Rob Ford, Pete Lord, Roger McWire");
+
+        System.out.println("CRIMINALS DATA:");
+        for (Map.Entry<String, String> criminal : criminals.entrySet()) {
+            System.out.println(buildMatchString(criminal.getKey(), criminal.getValue()));
+        }
+        System.out.println();
 
         // Add as many as you want
         System.out.println("Searching: " + "Red Fortress" + "\nFound -> " + findCriminal(criminals, "Red Fortress"));
@@ -27,88 +32,80 @@ public class Main {
         System.out.println("Searching: " + "Llong Jr" + "\nFound -> " + findCriminal(criminals, "Llong Jr"));
         System.out.println("Searching: " + "Ford" + "\nFound -> " + findCriminal(criminals, "Ford"));
         System.out.println("Searching: " + "paul White" + "\nFound -> " + findCriminal(criminals, "paul White"));
-        System.out.println("Searching: " + "Roger" + "\nFound -> " + findCriminal(criminals, "Roger"));
         System.out.println("Searching: " + "Ross" + "\nFound -> " + findCriminal(criminals, "Ross"));
         System.out.println("Searching: " + "white jr." + "\nFound -> " + findCriminal(criminals, "white jr."));
-        System.out.println("Searching: " + "rabbit" + "\nFound -> " + findCriminal(criminals, "rabbit"));
-        
+        System.out.println("Searching: " + "Fort" + "\nFound -> " + findCriminal(criminals, "Fort"));
+
+
     }
 
     /**
      * TODO: Implement
+     * No split() methods or secondary for-each loops are being used, which would
+     * increase the time complexity of the algorithm, even though the responses will
+     * not have the best quality.
      *
      * @param criminals
      * @param possibleName
      * @return
      */
     public static String findCriminal(Map<String, String> criminals, String possibleName) {
-        if (possibleName == null) {
+        if (possibleName == null || criminals == null) {
             return "No match";
         }
+        ArrayList<String> possibleMatches = new ArrayList<>();
         /**
-         * Checking all actual names.
+         * Looking for exact matches in all actual names.
          */
         for (Map.Entry<String, String> criminal : criminals.entrySet()) {
-            /**
-             * Tokenizes the key, to look for an exact match in each word of the actual name.
-             */
-            String[] words = criminal.getKey().split(" ");
-            for (String word : words) {
-                if (word.toLowerCase().equals(possibleName.toLowerCase())) {
-                    return buildMatch(criminal.getKey(), criminal.getValue());
-                }
+            if (criminal.getKey().equalsIgnoreCase(possibleName)) {
+                return buildMatchString(criminal.getKey(), criminal.getValue());
             }
         }
         /**
-         * Checking all aliases.
+         * Gathering all possible matches in the actual names.
          */
         for (Map.Entry<String, String> criminal : criminals.entrySet()) {
-            try {
-                /**
-                 * Tokenizes the whole value, so that it returns an array of composite aliases.
-                 */
-                String[] aliases = criminal.getValue().split(", ");
-                for (String alias : aliases) {
-                    /**
-                     * Tokenizes the value, to look for an exact match in each word of the alias.
-                     */
-                    String[] words = alias.split(" ");
-                    for (String word : words) {
-                        if (word.toLowerCase().equals(possibleName.toLowerCase())) {
-                            return buildMatch(criminal.getKey(), criminal.getValue());
-                        }
-                    }
-                }
-            } catch (NullPointerException npe){
-                // Treating empty aliases (values)
-            }
-        }
-        /**
-         * Checking for partial or exact matches with the actual names (keys).
-         */
-        for (Map.Entry<String, String> criminal : criminals.entrySet()) {
+            // Looking for a partial match (case-insensitive)
             if (criminal.getKey().toLowerCase().contains(possibleName.toLowerCase())) {
-                return buildMatch(criminal.getKey(), criminal.getValue());
+                possibleMatches.add(criminal.getKey());
             }
         }
         /**
-         * Checking for partial or exact matches with the aliases (values).
+         * Analyzing each possible match, looking for exact matches in the tokens
          */
+        for (String possibleMatch : possibleMatches) {
+            String[] nameTokens = possibleMatch.split(" ");
+            for (String token : nameTokens) {
+                if (token.equalsIgnoreCase(possibleName)){
+                    String aliases = criminals.get(possibleMatch);
+                    return buildMatchString(possibleMatch, aliases);
+                }
+            }
+        }
+        /**
+         * Checking for a partial match in the whole aliases' string (case-insensitive)
+         */
+        possibleMatches.clear();
         for (Map.Entry<String, String> criminal : criminals.entrySet()) {
-            if (criminal.getValue().toLowerCase().contains(possibleName.toLowerCase())) {
-                return buildMatch(criminal.getKey(), criminal.getValue());
+            if (criminal.getValue() == null) {
+                continue;
+            }
+            String aliasesLower = criminal.getValue().toLowerCase();
+            if (aliasesLower.contains(possibleName.toLowerCase())) {
+                return buildMatchString(criminal.getKey(), criminal.getValue());
             }
         }
         return "No match";
     }
 
-    public static String buildMatch(String actualName, String aliases) {
+    public static String buildMatchString(String actualName, String aliases) {
         String match = "";
-        match += "First name: " + actualName + ". ";
+        match += "First name: " + actualName + ". " + "Aliases: ";
         if (aliases != null)
-            match += "Aliases: " + aliases;
+            match += aliases;
         else
-            match += "Aliases: No aliases";
+            match += "No aliases";
         return match;
     }
 }
